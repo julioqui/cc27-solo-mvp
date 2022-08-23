1696ee3b145a368c44386e630b0e74a954117e6d
Julio Quiezi <julioquiezi@gmail.com>
2022-08-23 18:25:11 +0900

    fix: delete unused migration

---
+++ src/backend/model/data/migrations/20220823023432_add_password_column_to_owners_table2.js
@@ -0,0 +1,13 @@
+const TABLE_NAME = "owners";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void[]> }
+ */
+exports.up = async function (knex) {};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void[]> }
+ */
+exports.down = async function (knex) {};
